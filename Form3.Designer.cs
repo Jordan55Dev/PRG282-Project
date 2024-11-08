62f8f7b0a6f55c57356359ab10fe05952d543397
Troniccs2 <foxtocrazy@gmail.com>
2024-11-08 08:53:50 +0200

    button reg gemaak-Hanno

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -84,6 +84,7 @@
             this.button2.TabIndex = 9;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
